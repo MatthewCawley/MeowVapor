6d1752f3b145f16986b43606cd970d00958b5141
MatthewCawley <me@matthewcawley.co.uk>
2018-05-06 22:51:11 +0100

    Update Package.swift
    Updated dependency versions

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
             targets: ["MeowVapor"]),
         ],
     dependencies: [
-        .package(url: "https://github.com/OpenKitten/Meow.git", .revision("master/1.0")),
-        .package(url: "https://github.com/OpenKitten/MongoKitten.git", .revision("master/4.1")),
-        .package(url: "https://github.com/OpenKitten/BSON.git", .revision("master/5.2")),
-        .package(url: "https://github.com/vapor/vapor.git", from: "2.2.0")
+        .package(url: "https://github.com/OpenKitten/Meow.git", .branch("master/2.0")),
+        .package(url: "https://github.com/OpenKitten/MongoKitten.git", .branch("master/5.0")),
+        .package(url: "https://github.com/OpenKitten/BSON.git", .branch("master/6.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
